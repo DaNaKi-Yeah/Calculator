e5112a8a1bdf43d7b1f8f65c4a8c7b51bafa4371
local_DaNaKi_onPC <DaNaKi.Yeah@gmail.com>
2021-07-20 04:21:06 +0600

    make buttons in the main View, and try to make  function "click" on numbers.

---
+++ Calculator/ViewModels/MainViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows;
+
+namespace Calculator.ViewModels
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+        public MainViewModel()
+        {
+
+        }
+
+        private string field;
+        public string Field
+        {
+            get { return field; }
+            set { field = value; OnPropertyChanged("Field"); }
+        }
+
+
+        private RelayCommand inputNumberCommand;
+        public RelayCommand InputNumberCommand
+        {
+            get
+            {
+                return inputNumberCommand ??
+                  (inputNumberCommand = new RelayCommand(obj =>
+                  {
+                      Field += Convert.ToString(obj);
+                  }));
+            }
+        }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
